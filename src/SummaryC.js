0e503160975fee4a2abe5405e20973fef2cfeb19
vaddiharshita <113936704+vaddiharshita@users.noreply.github.com>
2022-10-26 16:36:28 +0530

    update

--- src/SummaryC.js
+++ src/SummaryC.js
@@ -1,23 +1,24 @@
 import React from "react";
-import './style/summaryC.css';
-export default function SummaryCount() {
+import './SummaryCount.css';
+import { BiTrendingUp } from "react-icons/bi"
+export default function SummaryCount({ pcount, value, show }) {
     return (
         <>
-            <div>
-                <div className="summarycourt">
+            <div className="summarycount_outer">
+                <div className="summarycount_inner">
+                    <div className="summarycount_inner_row1">
+                        <label className="summarycount_label">Prospect Growth</label>
+                    </div>
+                    <div className="summarycount_inner_row2">
+                        <label className="summarycount_value">#{pcount} </label>
+                    </div>
+                    {show ? (<div className="summarycount_inner_row3">
+                        <BiTrendingUp className="summarycount_inner_row3_icon" />
+                        <label> +{value}% </label>
+                    </div>) : <></>}
                 </div>
-                <div className="summarycourt1">
-                    <label>Prospect Growth</label>
-                </div>
-                <div>
-                    <label className="summarycount_value"># 500</label>
-                </div>
-
             </div>
-
         </>
     );
 
 }
-
-
